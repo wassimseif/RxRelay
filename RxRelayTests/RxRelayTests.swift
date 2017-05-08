79e271d2b9ada853075472395a6b388f78b02ebc
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2017-05-08 21:14:28 +0300

    Skeleton unit tests for publish relay

--- RxRelayTests/RxRelayTests.swift
+++ RxRelayTests/RxRelayTests.swift
@@ -14,6 +14,16 @@ class RxRelayTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var relay = PublishRelay<String>()
+        
+        let subscription = relay.subsribe { (string ) in
+            print("I have Received :\(string )")
+        }
+        
+        relay.accept("Hello")
+        relay.accept("World")
+        subscription.dispose()
+        relay.accept("Will not be emitted ")
     }
     
     override func tearDown() {
